389efe6c80064ac0498168005fb9baa17dc0b643
MBugajski <michalbugajski@outlook.com>
2021-01-29 23:58:22 +0100

    Added @InitBinder for white space validation

--- src/com/mbugajski/springdemo/mvc/StudentController.java
+++ src/com/mbugajski/springdemo/mvc/StudentController.java
@@ -5,9 +5,12 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,6 +20,12 @@ public class StudentController {
 	@Value("#{countryOptions}")
 	private Map<String, String> countryOptions;
 
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
 		Student theStudent = new Student();
